7b318492acc1d811ccb7996fe72efeea22fd556e
Omor Faruq <faruq05.fiverr@gmail.com>
2023-08-25 22:30:23 +0600

    d

--- js/select.js
+++ js/select.js
@@ -1,9 +1,23 @@
 // div selector
-
-$(document).ready(function () {
-    $('.selectable').click(function () {
-      $(this).toggleClass('selected');
-    });
+$(document).ready(function() {
+  $('.selectable').click(function() {
+    $('.selectable').not(this).removeClass('selected'); // Remove 'selected' class from other divs
+    $(this).toggleClass('selected'); // Toggle 'selected' class on the clicked div
   });
-  
-  
+});
+
+
+
+//text_copy
+$(document).ready(function() {
+  $('.copy-link').click(function(e) {
+    e.preventDefault(); // Prevent default hyperlink behavior
+    var textToCopy = $('#text-to-copy').text(); // Get the text to copy
+    var tempTextArea = $('<textarea>'); // Create a temporary textarea element
+    $('body').append(tempTextArea); // Append it to the DOM
+    tempTextArea.val(textToCopy).select(); // Set the value and select the text
+    document.execCommand('copy'); // Copy the selected text
+    tempTextArea.remove(); // Remove the temporary textarea
+    alert('Text copied: ' + textToCopy);
+  });
+});
